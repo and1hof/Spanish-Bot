c30f1ab0b3b187579eff4ad60ef3e7d31bc06165
Andrew Hoffman <andhofmt1993@gmail.com>
2016-02-09 02:38:01 +0000

    Integrated Devise for user authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#index'
+
+  get 'pages/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
