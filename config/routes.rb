ce4728a2d48c834dd93d5fa810cfeacdfcc91a87
Andrew Hoffman <andhofmt1993@gmail.com>
2016-02-09 03:06:08 +0000

    Integrated foundation SASS for styling - scaffolded out pages and navigation bars.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
 
-  get 'pages/home'
+  get '/home' => 'pages#home'
+  get '/chat' => 'pages#chat'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
